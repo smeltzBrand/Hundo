e12f490682db7e22e39e6c0b7f74a7b7d59e2df5
smeltzBrand <101213259+smeltzBrand@users.noreply.github.com>
2022-04-25 12:39:28 -0400

    Verify inputs, change colors and hrefs

--- js/site.js
+++ js/site.js
@@ -4,7 +4,17 @@ function getValues(){
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    // Check to see if user enter integers between 1 and 100
+    if (Number.isInteger(startValue) && Number.isInteger(endValue) && startValue >= 1 && startValue <= 100 && endValue >= 1 && endValue <= 100) {
+    
+
     displayValues(startValue, endValue);
+    } else {
+        Swal.fire("Oops!","Please enter integers between 1 and 100.")
+    }
 
 }
 
@@ -14,6 +24,7 @@ function displayValues(start, end) {
     
     //first get the ol element from the page
     element = document.getElementById("results");
+    element.innerHTML = "";
     
 for (let i = start; i <= end; i++) {
     
